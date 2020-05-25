2ffcae8e14ea3b8c122a13fdde598ac8ecace310
Kilian Eller <kilianeller12@outlook.com>
2020-05-25 18:56:50 +0200

    use class Command to send commands instead of simple uint list

---
+++ lib/Command.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'Command.g.dart';
+
+@JsonSerializable()
+class Command {
+  int outDataLength;
+  List<int> inData;
+
+  Command(this.outDataLength, this.inData);
+
+  /*
+   * Json to Location object
+   */
+  factory Command.fromJson(Map<String, dynamic> json) =>
+      _$CommandFromJson(json);
+
+  /*
+   * Location object to json
+   */
+  Map<String, dynamic> toJson() => _$CommandToJson(this);
+}
