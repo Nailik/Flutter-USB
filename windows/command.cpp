dd635cb29a950d7283430550ce7db25000b5b206
Kilian Eller <kilianeller12@outlook.com>
2020-07-03 23:53:44 +0200

    fixed warning treated as error

--- windows/command.cpp
+++ windows/command.cpp
@@ -4,5 +4,5 @@ Command::Command(std::vector<uint8_t>& value, int new_result_length)
 {
     byte_list = value;
     result_length = new_result_length;
-    command_length = byte_list.size();
+    command_length = (int)byte_list.size();
 }
